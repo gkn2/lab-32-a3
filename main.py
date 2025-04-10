f20b15acde2587ccc1fe8065d6c2cd9991998121
gkn2 <112503427+gkn2@users.noreply.github.com>
2025-04-10 11:55:20 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ def find_length(name):
     letters = len(name)
     return letters
 
+
+
 def change_greeting(name):
     letters = 0
     letters = find_length(name)
     return "Length = " + str(letters)
 
+
+
 mySprite = sprites.create(assets.image("""mc"""), SpriteKind.player)
+
+
 
 mySprite.say(change_greeting("Pat"))
